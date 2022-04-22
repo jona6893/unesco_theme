44552c21c1820b99b9fd3cd035284bfba5c3de32
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2022-04-22 14:56:13 +0200

    js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-/*
 document.addEventListener('DOMContentLoaded', () => {
 
 	console.log("hello world");
 
 	const header = document.getElementById("masthead");
-	const logo = document.querySelector(".header-logo-wrapper")
+	const navbar = document.getElementById("site-navigation");
+	const logo = document.querySelector(".header-logo-wrapper");
 
 	console.log(header)
 
@@ -15,12 +15,17 @@ document.addEventListener('DOMContentLoaded', () => {
 	if (header.classList.contains('headroom--not-top')) {
 		console.log("Its unpinned")
 		logo.classList.add("hvidlogo")
+	
+		
 	}
 		else {
 		console.log("its pinned")
 		logo.classList.remove("hvidlogo")
 	}
 		
+		
+		
+		
 	});
 });
-*/
+
