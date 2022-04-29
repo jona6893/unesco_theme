ba1955ba87a012976d4fab86cd96f15b12c0bac6
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2022-04-29 18:12:53 +0200

    måske de sidste ændringer?

--- page-undervisningsmarteriale.php
+++ page-undervisningsmarteriale.php
@@ -15,7 +15,7 @@ get_header();
 		<main id="main" class="site-main">
 	<section id="listgrid">
 
-	
+	<button class="col_btn">Skoletrin</button>
 	<div id="drop-down">
 	<button data-skoletrinet="alle" class="valgt" >Alle</button>
 	</div>
@@ -51,6 +51,84 @@ get_header();
 
 </div><!-- #div -->
 
+<style>
+      @media (min-width: 615px) {
+        #footer {
+          --repeat: 3;
+        }
+      }
+
+      #footer {
+        padding: 0%;
+padding-bottom: 5%;
+    padding-top: 5%;
+        margin: 0%;
+        width: 100%;
+        height: auto;
+        background-color: #53abcebf;
+        display: grid;
+        grid-template-columns: repeat(
+          var(--repeat, auto-fit),
+          minmax(200px, 1fr)
+        );
+        color: white;
+        align-items: center;
+      }
+      #footer a {
+        color: white;
+        text-decoration: none;
+      }
+      #footer_kontakt {
+        display: grid;
+        justify-content: center;
+        padding: 3%;
+      }
+      .footer_heading {
+        color: white;
+      }
+      #footer_logo {
+        width: 100%;
+        filter: brightness(0) invert(1);
+        display: grid;
+        justify-content: center;
+        padding: 3%;
+      }
+      #footer_some {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 15px;
+        justify-content: center;
+        padding: 3%;
+      }
+      #footer_some img {
+        max-width: 10%;
+        min-width: 5%;
+      }
+    </style>
+  
+  
+    <section id="footer">
+      <div id="footer_logo">
+        <img src="https://meritfilm.dk/kea/09_cms/test_site/wordpress/wp-content/uploads/2022/04/unesco_logo-1.webp" alt="">
+      </div>
+      <div id="footer_kontakt">
+        <article>
+          <h3 class="footer_heading">National Koordinator</h3>
+          <h4 class="footer_heading">Poul Erik Christoffersen</h4>
+          <address>
+            <a href="pec@ungdomsbyen.dk">pec@ungdomsbyen.dk</a><br><br>Tlf:
+            +45 4491 4646 <br><br>Direkte: +45 2174 4275
+          </address>
+        </article>
+      </div>
+      <div id="footer_some">
+        <img src="https://meritfilm.dk/kea/09_cms/test_site/wordpress/wp-content/uploads/2022/04/facebook1.png" alt="">
+        <img src="https://meritfilm.dk/kea/09_cms/test_site/wordpress/wp-content/uploads/2022/04/instagram2.png" alt="">
+        <img src="https://meritfilm.dk/kea/09_cms/test_site/wordpress/wp-content/uploads/2022/04/youtube.png" alt="">
+        <img src="https://meritfilm.dk/kea/09_cms/test_site/wordpress/wp-content/uploads/2022/04/tiktok.png" alt="">
+      </div>
+    </section>
+
 <
 
 <script>
@@ -72,7 +150,10 @@ get_header();
 	function start() {
 		console.log("start");
 		getJson();
-	
+		
+		document.querySelector("#drop-down").classList.add("disnone")
+		document.querySelector(".alleknappen").classList.add("disnone")
+		
 	}
 
 const url = `https://meritfilm.dk/kea/09_cms/test_site/wordpress/wp-json/wp/v2/uvmaterialer?per_page=100`;
@@ -94,6 +175,12 @@ async function getJson() {
 	opretKnapper();
 
 }
+
+document.querySelector(".col_btn").addEventListener("click", () =>{
+	document.querySelector("#drop-down").classList.toggle("disnone")
+})
+
+
 function opretKnapper() {
 categories.forEach(cat=> {
 		document.querySelector("#drop-down").innerHTML += `<button class="filterKnapper" data-skoletrinet="${cat.id}">${cat.name}</button>`
@@ -104,14 +191,21 @@ categories.forEach(cat=> {
 		document.querySelector("#drop-down2").innerHTML += `<div class="dropdown2style"><div class="${classes + number}" ></div><button class="filterKnapper2"  data-verdensml="${maal.id}">${maal.name}</button></div>`
 	});	
 		
-	
+	document.querySelectorAll(".dropdown2style").forEach(bnt=>{bnt.classList.add("disnone")})
 	addEventListenersToButtons();
 }
 
 
+
 function addEventListenersToButtons() {
+	document.querySelector("#tekst_til_filterering").addEventListener("click", () =>{
+	document.querySelector(".alleknappen").classList.toggle("disnone")
+	document.querySelectorAll(".dropdown2style").forEach(btn=>{
+		btn.classList.toggle("disnone")
+	})
+});
 document.querySelectorAll("#drop-down button").forEach(elm => {elm.addEventListener("click", filtrering);
-})	
+});
 document.querySelectorAll("#drop-down2 button").forEach(elm => {elm.addEventListener("click", filtrering2);	
 })
 };
@@ -144,7 +238,7 @@ function visUmaterale() {
     	Umaterale.forEach(materale => {
 			
 		/* if (filterskoletrin == "alle" || materale.skoletrinet[0].id === (parseInt(filterskoletrin)) || materale.verdensml.includes(parseInt(filterverdensmaal)) ){ */
-		if (filterskoletrin == "alle" || materale.skoletrinet[0].id === (parseInt(filterskoletrin)) && filterverdensmaal == "alle" || materale.verdensml.includes(parseInt(filterverdensmaal))){
+		if (materale.skoletrinet[0].id === (parseInt(filterskoletrin)) || filterskoletrin == "alle" && filterverdensmaal == "alle" || materale.verdensml.includes(parseInt(filterverdensmaal))){
 		/* console.log(filterskoletrin)*/
 		console.log(filterverdensmaal) 
         let klon = temp.cloneNode(true).content;
--- single-uvmaterialer.php
+++ single-uvmaterialer.php
@@ -10,8 +10,9 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-
+    <button class="backbtn">Tilbage</button>
       <article id="singleView-slik">
+
         <img class="pic" src="" alt=""/>
         <div>
         <h2></h2>
@@ -22,7 +23,85 @@ get_header();
       </article>
     
 
-<button>Tilbage</button>
+<style>
+      @media (min-width: 615px) {
+        #footer {
+          --repeat: 3;
+        }
+      }
+
+      #footer {
+        padding: 0%;
+padding-bottom: 5%;
+    padding-top: 5%;
+        margin: 0%;
+        width: 100%;
+        height: auto;
+        background-color: #53abcebf;
+        display: grid;
+        grid-template-columns: repeat(
+          var(--repeat, auto-fit),
+          minmax(200px, 1fr)
+        );
+        color: white;
+        align-items: center;
+      }
+      #footer a {
+        color: white;
+        text-decoration: none;
+      }
+      #footer_kontakt {
+        display: grid;
+        justify-content: center;
+        padding: 3%;
+      }
+      #footer_kontakt,
+      h3,
+      h4 {
+        color: white !important;
+      }
+      #footer_logo {
+        width: 100%;
+        filter: brightness(0) invert(1);
+        display: grid;
+        justify-content: center;
+        padding: 3%;
+      }
+      #footer_some {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 15px;
+        justify-content: center;
+        padding: 3%;
+      }
+      #footer_some img {
+        max-width: 10%;
+        min-width: 5%;
+      }
+    </style>
+  
+  
+    <section id="footer">
+      <div id="footer_logo">
+        <img src="https://meritfilm.dk/kea/09_cms/test_site/wordpress/wp-content/uploads/2022/04/unesco_logo-1.webp" alt="">
+      </div>
+      <div id="footer_kontakt">
+        <article>
+          <h3>National Koordinator</h3>
+          <h4>Poul Erik Christoffersen</h4>
+          <address>
+            <a href="pec@ungdomsbyen.dk">pec@ungdomsbyen.dk</a><br><br>Tlf:
+            +45 4491 4646 <br><br>Direkte: +45 2174 4275
+          </address>
+        </article>
+      </div>
+      <div id="footer_some">
+        <img src="https://meritfilm.dk/kea/09_cms/test_site/wordpress/wp-content/uploads/2022/04/facebook1.png" alt="">
+        <img src="https://meritfilm.dk/kea/09_cms/test_site/wordpress/wp-content/uploads/2022/04/instagram2.png" alt="">
+        <img src="https://meritfilm.dk/kea/09_cms/test_site/wordpress/wp-content/uploads/2022/04/youtube.png" alt="">
+        <img src="https://meritfilm.dk/kea/09_cms/test_site/wordpress/wp-content/uploads/2022/04/tiktok.png" alt="">
+      </div>
+    </section>
 
     </main>
     <script>
@@ -46,14 +125,14 @@ get_header();
 
 
       function visSlikket() {
-document.querySelector("h2").textContent = materale.title.rendered;
+document.querySelector("h2").textContent = materale.overskrift_singleview;
  document.querySelector(".pic").src = materale.billede.guid;
- document.querySelector(".smag").textContent = slik.smag;
-  document.querySelector(".beskrivelse").textContent = slik.beskrivelse;
-  document.querySelector(".pris").textContent = slik.pris + " kr.";
+ document.querySelector(".smag").textContent = "Forkuspunkt " + materale.fokuspunkt;
+  document.querySelector(".beskrivelse").textContent = materale.lang_beskrivelse;
+  /* document.querySelector(".pris").textContent = slik.pris + " kr."; */
       }
 
-      document.querySelector("button").addEventListener("click", () => {
+      document.querySelector(".backbtn").addEventListener("click", () => {
         history.back();
       });
       
